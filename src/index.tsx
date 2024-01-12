6474e2ac038871608cf3d3fcaae465634518bf5b
debasishrowlo <debasishrowlo@gmail.com>
2024-01-12 11:33:54 +0530

    Update level 4 instruction

--- src/index.tsx
+++ src/index.tsx
@@ -227,7 +227,7 @@ const levels:Level[] = [
     goals: [{ x: 10, y: 2 }],
   },
   {
-    popupMessage: `Press "R" to reset a level`,
+    popupMessage: "Press multiple switches at once",
     tilemap: [
       2, 2, 2, 2, 2, 2, 2, 2, 2,
       2, 1, 1, 1, 1, 1, 2, 1, 2,
